d2da8348f8e339ac2249a2503ed297d0cc595086
Patrick Palczewski <psykiatris@gmail.com>
2025-02-28 06:29:59 -0800

    Unsuccessfully tried to set up a record Account to contain data. Desired to make separate classes for the different loan types and use Acdcount to hold data to process later.

--- fsaccounts/src/main/java/baseaccount/CreditCard.java
+++ fsaccounts/src/main/java/baseaccount/CreditCard.java
@@ -1,10 +1,14 @@
 package baseaccount;
 
 public class CreditCard extends AccountInfo {
+    public final int maxTerm = 24;   // range from 0-24 months
+
 
     public CreditCard(double p, double r){
 
         super(p, r);
         this.name = "Credit Card";
+
+
     }
 }
--- src/RunRun.java
+++ src/RunRun.java
@@ -4,9 +4,10 @@ import baseaccount.CreditCard;
 
 public class RunRun {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
         CreditCard cc = new CreditCard(1000, 24.49);
         System.out.println(cc.name);
+        System.out.println("Term: " + cc.maxTerm + " months.");
 
 
     }
